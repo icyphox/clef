8015193be5181acacd91b06ada61f91ef844391f
Anirudh <icyph0x@pm.me>
2018-08-08 19:02:50 +0530

    fix: handle IndexError

--- src/clef.nim
+++ src/clef.nim
@@ -13,18 +13,24 @@ var cmd: seq[string]
 while true:
   var r = client.recvLine()
   cmd = r.split()
-  case cmd[0]:
-    of "set":
-      data.add(cmd[1], cmd[2])
-      echo("you set ", data)
-    of "get":
-      try:
-        echo(data[cmd[1]])
-      except KeyError:
-        echo("error: no such key '$#'" % cmd[1])
-    else:
-      echo("invalid command $#" % $cmd[0])
-      break
+  try:
+    case cmd[0]:
+      of "set":
+        if(cmd[2] == ""):
+          echo("can't set nothing")
+          break
+        data.add(cmd[1], cmd[2])
+        echo("you set ", data)
+      of "get":
+        try:
+          echo(data[cmd[1]])
+        except KeyError:
+          echo("error: no such key '$#'" % cmd[1])
+      else:
+        echo("invalid command $#" % $cmd[0])
+  except IndexError:
+    echo("error: $# expects an argument" % cmd[0])
+
 
 client.close()
 server.close()
